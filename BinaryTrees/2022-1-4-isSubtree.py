55f9d07e80c6b581c1c6a30f571988c107187cab
Wyxbqsj <41176885+Wyxbqsj@users.noreply.github.com>
2022-01-04 20:29:47 +0800

    另一棵树的子树

---
+++ BinaryTrees/2022-1-4-isSubtree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSubtree(self, root: TreeNode, subRoot: TreeNode) -> bool:
+        def isSametree(p,q):
+            if not p and not q:
+                return True
+            elif not p or not q:
+                return False
+            else:
+                if p.val==q.val:
+                    return isSametree(p.left,q.left) and isSametree(p.right,q.right)
+                else:
+                    return False
+        if not root:
+            return False
+        return self.isSubtree(root.left,subRoot) or self.isSubtree(root.right,subRoot) or isSametree(root,subRoot)
+            
+            
+            
+            
